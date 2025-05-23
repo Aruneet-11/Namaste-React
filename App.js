78f3d3cbb11c4b2e0256b8b533f9d154c677418b
Aruneet <87275550+Aruneet-11@users.noreply.github.com>
2025-05-23 11:23:07 +0530

    React Functional Components

--- App.js
+++ App.js
@@ -1,4 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id:"new"}, "Hello World from React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+//using JSX.
+//HTML like structure.
+//Babel converts this jsx into ReactElement and then into JS object after that It will render as an HTML DOM element onto the root DOM.
+//React Element
+const title= <h1 id="new">Namaste React using JSX!</h1>
+
+//React Functional Component.
+//Functional component is the normal JS function that returns a JSX Element or React Element.
+const HeadingComponent=()=>{
+  return ( <div>{title}
+  <h1>Namaste React from Functional Component!</h1>
+  </div>)
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//Render Functional Component
+root.render(<HeadingComponent/>);
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 #Namaste React
+
+#Parcel
+-dev build
+-local server
+-HMR = Hot Module Replacement(Refreshing the page immediately)
+-File watching algo writeen in C++
+-Caching-faster builds
+-Image optimization
+-Minification
+-Bundling
+-Compressed
